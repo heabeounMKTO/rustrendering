fa9f20f1391a7e1d98e6991578db6b64819e1b24
DESKTOP-PEGPU1O\hbwindows <soreach082.infinity@gmail.com>
2023-06-24 18:00:52 +0700

    x

--- src/mafs/camera.rs
+++ src/mafs/camera.rs
@@ -1,4 +1,4 @@
-use super::{vec::Vec3, ray::Ray};
+use super::{vec::Vec3, ray::Ray, mafsconsts};
 
 pub struct Camera{
     pub view_h: f64,
@@ -7,18 +7,26 @@ pub struct Camera{
     pub focal_length: f64,
     pub horizontal: Vec3,
     pub vertical: Vec3,
-    pub lower_left_c: Vec3
+    pub lower_left_c: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3
 }
 
 impl Camera{
-    pub fn new(view_h: f64, 
-               view_w: f64,
-               origin: Vec3, 
-               focal_length: f64) -> Camera{
+    pub fn new(look_from: Vec3,
+              look_at: Vec3,
+              vup: Vec3, 
+              vfov: f64,
+              aspect: f64 
+               ) -> Camera{
+               let theta = vfov*mafsconsts::PI / 180.0;
                let horizontal: Vec3 = Vec3::new(view_w,0.0,0.0);
                let vertical: Vec3 = Vec3::new(0.0, view_h, 0.0);
                let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
-                return Camera{
+               
+               
+               return Camera{
                     view_h: view_h,
                     view_w: view_w,
                     origin: origin,
